e89e23d5a15fbe86066e2e82fbba277d2c863b09
Valentina Andrada Perino <vandradaperino@frba.utn.edu.ar>
2026-04-22 18:12:01 -0300

    Implement basic layout with image, title and rating

--- test/ContentView.swift
+++ test/ContentView.swift
@@ -9,13 +9,57 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack{
+            Color(.blue) //color background
+                .ignoresSafeArea() //para que quede el color del background incluido en la parte del dinamic island y el botón
+            
+            // Tarjeta
+            VStack (alignment: .leading, spacing: 20) {
+                Image("niagarafalls")
+                    .resizable()
+                    .scaledToFit() // lo mismo que poner: .aspectRatio(contentMode: .fit)
+                    .clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous))
+                
+                HStack{
+                    Text("Niagara Falls")
+                        .font(.title)
+                        .bold()
+                    
+                    Spacer() //funciona como space-between entre titulo y estrellas
+                    
+                    VStack{
+                        HStack{
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.leadinghalf.filled")
+                        }.foregroundStyle(.orange)
+                        
+                        Text("(Reviews 361)")
+                            .font(.caption)
+                    }
+                }
+                Text("Come visit for an experience of a lifetime.")
+                
+                HStack{
+                    Spacer()
+                    Image(systemName: "fork.knife")
+                    Image(systemName: "binoculars.fill")
+                }
+                .foregroundStyle(.gray)
+                .font(.caption)
+            }
+            .padding()
+            .background(){
+                Rectangle()
+                    .foregroundStyle(.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 14, style: .continuous))
+                    .shadow(radius: 13)
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
