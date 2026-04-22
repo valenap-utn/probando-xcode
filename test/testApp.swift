de30168f01e0dade6d4b74558ec29d9c18588f83
Valentina Andrada Perino <vandradaperino@frba.utn.edu.ar>
2026-04-22 16:17:17 -0300

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Valentina Andrada Perino on 22/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
